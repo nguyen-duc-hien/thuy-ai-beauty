1a4f52a5b78b8a045406f1088430f26da6a64a48
Nguyen Duc Hien <ndhyuri@gmail.comn>
2025-03-29 10:39:54 +0700

    add new home

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -8,24 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
             document.querySelectorAll(".accordion-item").forEach(otherItem => {
                 if (otherItem !== item) {
                     otherItem.classList.remove("active");
-                    otherItem.querySelector(".accordion-title").classList.remove("text-neutralc-800");
-                    otherItem.querySelector(".accordion-show-more img").src = "/assets/image/icon/plus-round-white.svg";
                 }
             });
 
             // Toggle trạng thái active
             this.classList.toggle("active");
 
-            // Kiểm tra nếu active thì đổi class & icon
-            if (this.classList.contains("active")) {
-                // title.classList.remove("text-white");
-                title.classList.add("!text-neutralc-800");
-                img.src = "/assets/image/icon/minus-round-primary.svg";
-            } else {
-                title.classList.remove("!text-neutralc-800");
-                // title.classList.add("text-white");
-                img.src = "/assets/image/icon/plus-round-white.svg";
-            }
         });
     });
 });
